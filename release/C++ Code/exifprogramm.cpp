a68e3bf23f4d1721f4bb606cf178383c66f0994f
Seb. Hao <sebastianhao38478@gmail.com>
2026-06-10 20:28:59 +0200

    Relative Pfad mit Bildanzeige ist implementiert

--- release/C++ Code/exifprogramm.cpp
+++ release/C++ Code/exifprogramm.cpp
@@ -11,6 +11,35 @@
 using namespace std;
 namespace fs = std::filesystem;
 
+//*Anzeigt die Bilder in Ordner beim eingabe relativer Pfad
+void showImages(const fs::path& currentDir)
+{
+    int count = 0;
+
+    for (const auto& entry : fs::directory_iterator(currentDir))
+    {
+        if (!entry.is_regular_file())
+            continue;
+
+        string ext = entry.path().extension().string();
+
+        transform(ext.begin(), ext.end(),
+                  ext.begin(), ::tolower);
+
+        if (ext == ".jpg"  ||
+            ext == ".jpeg" ||
+            ext == ".png"  ||
+            ext == ".tif"  ||
+            ext == ".tiff")
+        {
+            count++;
+            cout << count << ". "
+                 << entry.path().filename().string()
+                 << '\n';
+        }
+    }
+}
+
 string createEditableCopy(const string& originalPath) {
 
     fs::path original(originalPath);
@@ -92,133 +121,159 @@ void exportExifToFile(
 
 //!Hauptfunktion
 int main() {
-while (true) { //*Schleife über gesamte Programm
-    
-    string filename;
+    fs::path currentDir = fs::current_path();
+    while (true) { //*Schleife über gesamte Programm
 
-    cout << "\nBilddatei Verzeichnis eingeben\n";
-    cout << "(oder 'exit' zum Beenden):\n";
+        cout << "\nAktueller Ordner:\n";
+        cout << currentDir << "\n\n";
 
-    getline(cin, filename);
+        showImages(currentDir);
 
-    filename = removeQuotes(filename);
+        string filename;
 
-    if (filename == "exit")
-        return 0;
+        cout << "\nBilddatei oder Ordner Verzeichnis eingeben\n";
+        cout << "(.. eine Ebene nach oben oder 'exit' zum Beenden):\n";
+        
+        getline(cin, filename);
 
-    shared_ptr<Exiv2::Image> image;
-    
-    try{
-        image = Exiv2::ImageFactory::open(filename);
-    }
-    catch (const Exiv2::Error& e) {
-        cout << "Datei konnte nicht geoffnet werden (Exiv2-Fehler):\n"<< e.what() << "\n";
-        continue; // zurück zur Dateiauswahl
-    }  
-    if (image.get() == 0) {
-        cout << "Datei konnte nicht geoffnet werden.\n";
-        continue;
-    } 
+        filename = removeQuotes(filename);
 
-    // Original bleibt unangetastet
-    string backup = createEditableCopy(filename);
-
-    image->readMetadata(); //*Lesen von Exif Daten
-    
-    Exiv2::ExifData& exifData = image->exifData(); //*Speichern von Exif Daten in Buffer
-
-    if (exifData.empty()) { //*Fehler: Daten wurden nicht gefunden
-        cout << "Keine EXIF Daten gefunden.\n";
-        continue;
-    }
-
-    printExifData(exifData, exiftoUser); //*Datenausgabe
-
-    //*Aktion wählen
-    string command;
-
-    while(true){ //*Schleife über Datei bearbeitung
-        cout << "\nBefehl eingeben:\n";
-        cout << "lesen / schreiben / loeschen / export / zuruck / exit\n";
-
-        getline(cin, command);
-        command = toLower(command); //Prüft eingegebenen Text auf Großbuchstaben
-            
-        if (command == "zuruck") //Gang zurück zum Datei Auswahl
-            break;
-
-        if (command == "exit") //Beendet das gesamte Programm
+        if (filename == "exit")
             return 0;
 
-        //!Block zur Bearbeitung von Daten
-        try { 
+        fs::path path(filename);
 
-            if (command == "lesen"){ //*Daten werden gedruckt
-                printExifData(exifData, exiftoUser);
-            }
-            else if (command == "schreiben") { //*Daten werden umgeschrieben
-                string key;
-                string value;
+        //* Relative Pfade erlauben
 
-                cout << "EXIF Name:\n";
-                getline(cin, key);
-                key = toLower(key);
-
-                cout << "Neue Wert:\n";
-                getline(cin, value);
-                
-                if (fieldType[key] == "date")
-                    value = normaldate(value);
-
-                if (userToExif.find(key) != userToExif.end()) 
-                {
-                    setExif(*image, userToExif[key], value);
-                    image->readMetadata();
-                }
-                else{
-                    try { //*Schreibt neues Exif Key
-                        Exiv2::ExifKey exifKey(key);
-                        setExif(*image, key, value);
-                        image->readMetadata();
-                        cout << "Neuer Tag wurde erstellt.\n";
-                    }
-                    catch (...) {
-                        cout << "Unbekannter EXIF Name.\n";
-                    }
-                } 
-            }
-            else if (command == "loeschen") { //*Daten werden gelöscht
-                string key;
-
-                cout << "EXIF Name:\n";
-                getline(cin, key);
-                key = toLower(key);
-                if (userToExif.find(key) != userToExif.end()) 
-                {
-                    removeExif(*image, userToExif[key]);
-                    image->readMetadata();
-                }
-                else 
-                    cout << "Unbekannter EXIF Name.\n"; 
-            }
-            else if(command == "export"){
-                exportExifToFile(exifData, exiftoUser);
-            }
-            else
-                cout << "Unbekannter Befehl.\n";
-
-            }
-        catch (const Exiv2::Error& e) { //*Fehler von Bibliothek
-
-            cout << "\nEXIV2 FEHLER:\n";
-            cout << e.what() << "\n";
+        if (filename == "..")
+        {
+            currentDir = currentDir.parent_path();
+            continue;
         }
-        catch (const exception& e) { //*Allgemeine Fehler
-            cout << "\nALLGEMEINER FEHLER:\n"; 
-            cout << e.what() << "\n";
+
+        if (path.is_relative()) {
+            path = currentDir / path;
+        }
+
+        if (fs::is_directory(path))
+        {
+            currentDir = path;
+            continue;
+        }
+
+        shared_ptr<Exiv2::Image> image;
+        
+        try{
+            image = Exiv2::ImageFactory::open(filename);
+        }
+        catch (const Exiv2::Error& e) {
+            cout << "Datei konnte nicht geoffnet werden (Exiv2-Fehler):\n"<< e.what() << "\n";
+            continue; // zurück zur Dateiauswahl
+        }  
+        if (image.get() == 0) {
+            cout << "Datei konnte nicht geoffnet werden.\n";
+            continue;
+        } 
+
+        //* Macht eine Kopie von Origilandatei
+        string backup = createEditableCopy(filename);
+
+        image->readMetadata(); //*Lesen von Exif Daten
+        
+        Exiv2::ExifData& exifData = image->exifData(); //*Speichern von Exif Daten in Buffer
+
+        if (exifData.empty()) { //*Fehler: Daten wurden nicht gefunden
+            cout << "Keine EXIF Daten gefunden.\n";
+            continue;
+        }
+
+        printExifData(exifData, exiftoUser); //*Datenausgabe
+
+        //*Aktion wählen
+        string command;
+
+        while(true){ //*Schleife über Datei bearbeitung
+            cout << "\nBefehl eingeben:\n";
+            cout << "lesen / schreiben / loeschen / export / zuruck / exit\n";
+
+            getline(cin, command);
+            command = toLower(command); //Prüft eingegebenen Text auf Großbuchstaben
+                
+            if (command == "zuruck") //Gang zurück zum Datei Auswahl
+                break;
+
+            if (command == "exit") //Beendet das gesamte Programm
+                return 0;
+
+            //!Block zur Bearbeitung von Daten
+            try { 
+
+                if (command == "lesen"){ //*Daten werden gedruckt
+                    printExifData(exifData, exiftoUser);
+                }
+                else if (command == "schreiben") { //*Daten werden umgeschrieben
+                    string key;
+                    string value;
+
+                    cout << "EXIF Name:\n";
+                    getline(cin, key);
+                    key = toLower(key);
+
+                    cout << "Neue Wert:\n";
+                    getline(cin, value);
+                    
+                    if (fieldType[key] == "date")
+                        value = normaldate(value);
+
+                    if (userToExif.find(key) != userToExif.end()) 
+                    {
+                        setExif(*image, userToExif[key], value);
+                        image->readMetadata();
+                    }
+                    else{
+                        try { //*Schreibt neues Exif Key
+                            Exiv2::ExifKey exifKey(key);
+                            setExif(*image, key, value);
+                            image->readMetadata();
+                            cout << "Neuer Tag wurde erstellt.\n";
+                        }
+                        catch (...) {
+                            cout << "Unbekannter EXIF Name.\n";
+                        }
+                    } 
+                }
+                else if (command == "loeschen") { //*Daten werden gelöscht
+                    string key;
+
+                    cout << "EXIF Name:\n";
+                    getline(cin, key);
+                    key = toLower(key);
+                    if (userToExif.find(key) != userToExif.end()) 
+                    {
+                        removeExif(*image, userToExif[key]);
+                        image->readMetadata();
+                    }
+                    else 
+                        cout << "Unbekannter EXIF Name.\n"; 
+                }
+                else if(command == "export"){
+                    exportExifToFile(exifData, exiftoUser);
+                }
+                else
+                    cout << "Unbekannter Befehl.\n";
+
+                }
+            catch (const Exiv2::Error& e) { //*Fehler von Bibliothek
+
+                cout << "\nEXIV2 FEHLER:\n";
+                cout << e.what() << "\n";
+            }
+            catch (const exception& e) { //*Allgemeine Fehler
+                cout << "\nALLGEMEINER FEHLER:\n"; 
+                cout << e.what() << "\n";
+            }
         }
     }
-}
 
 cout << "\nProgramm beendet.\n";
 
